f1f0c31c70f7bc428382a11f4dc49c4767880b20
Thinh Dong <vanthinhgvo@gmail.com>
2025-08-13 23:48:12 +0700

    feat(register): Add registration function

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,5 +1,35 @@
 import background from '../assets/images/logo/bannerLogin.jpg';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axiosInstance from '../api/axios.js';
 function Register() {
+    const navigate = useNavigate();
+    const initialFormData = Object.freeze({
+        username: '',
+        password: '',
+        confirmPassword: '',
+    });
+    const [formData, updateFormData] = useState(initialFormData);
+    const handleChange = (e) => {
+        updateFormData({
+            ...formData,
+            [e.target.name]: e.target.value.trim(),
+        });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        axiosInstance 
+            .post('/users/register/', {
+                username: formData.username,
+                password: formData.password,
+            })
+            .then((res) => {
+                navigate('/login');
+                console.log(res);
+                console.log(res.data);
+            })
+    }
     return (
         <div className="flex justify-center items-center h-screen">
             <div className="w-1/2 h-full bg-cover bg-center" style={{ backgroundImage: `url(${background})` }}>
@@ -14,17 +44,17 @@ function Register() {
                     <form className="flex flex-col gap-2 w-full">
                         <div className="flex flex-col gap-2">
                             <label htmlFor="username">Username</label>
-                            <input className="border-2 border-gray-300 rounded-md p-2 focus:border-blue-500 focus:outline-none" type="text" id="username" name="username" />
+                            <input className="border-2 border-gray-300 rounded-md p-2 focus:border-blue-500 focus:outline-none" type="text" id="username" name="username" onChange={handleChange} />
                         </div>
                         <div className="flex flex-col gap-2">
                             <label htmlFor="password">Password</label>
-                            <input className="border-2 border-gray-300 rounded-md p-2 focus:border-blue-500 focus:outline-none" type="password" id="password" name="password" />
+                            <input className="border-2 border-gray-300 rounded-md p-2 focus:border-blue-500 focus:outline-none" type="password" id="password" name="password" onChange={handleChange} />
                         </div>
                         <div className="flex flex-col gap-2">
                             <label htmlFor="confirmPassword">Confirm Password</label>
-                            <input className="border-2 border-gray-300 rounded-md p-2 focus:border-blue-500 focus:outline-none" type="password" id="confirmPassword" name="confirmPassword" />
+                            <input className="border-2 border-gray-300 rounded-md p-2 focus:border-blue-500 focus:outline-none" type="password" id="confirmPassword" name="confirmPassword" onChange={handleChange} />
                         </div>
-                        <button className="bg-blue-500 text-white rounded-md p-2 mt-4 hover:bg-blue-600" type="submit">Register</button>
+                        <button className="bg-blue-500 text-white rounded-md p-2 mt-4 hover:bg-blue-600" type="submit" onClick={handleSubmit}>Register</button>
                     </form>
                     <div className="flex items-center justify-center gap-2 mt-4">
                         <label className="text-sm text-gray-500">Already have an account?</label>
